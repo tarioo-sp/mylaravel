d90e69893474b9481414a3fac056fa263f9f3e38
Sumeth Pinthong <sumeth.tar@gmail.com>
2021-04-30 08:52:25 +0700

    Commit laravel Frontend

---
+++ index_.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>My Laravel</title>
+
+  <style>
+    * {
+      font-family: verdana;
+      font-size: 10pt;
+      color: gray;
+    }
+    .centerwindow {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100vw;
+      height: 50vh;
+    }
+    b {
+      font-weight: bold;
+    }
+    table {
+      border: 1px solid gray;
+    }
+    td {
+      text-align: center;
+      padding: 25px;
+    }
+  </style>
+
+</head>
+
+<body>
+
+  <div class="centerwindow">
+    <table>
+      <tr>
+        <td>This is a placeholder for the subdomain <b>local.mylaravel.host</b></td>
+      </tr>
+    </table>
+  </div>
+
+</body>
+
+</html>
